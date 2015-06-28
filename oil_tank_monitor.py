cc282c9a94674875c700882aae4f793e1c7ade0f
kivster <kivster@users.noreply.github.com>
2015-06-28 13:46:29 +0100

    First commit of oil tank monitor

--- oil_tank_monitor.py
+++ oil_tank_monitor.py
@@ -5,22 +5,20 @@ import time
 import RPi.GPIO as GPIO
 import urllib
 
-# Constants
+# Defaults
 number_of_pings = 30
 oil_tank_height = 98
 oil_tank_length = 178
 oil_tank_width = 138
 oil_tank_capacity = 1200
-oil_burn_rate = 1.41
 ping_timeout = 0.02
-domoticz_device_idx = 13
 
 def measure():
     distances = []
     for ping in range(1, number_of_pings):
         complete=False
         while not complete:
-	    # Send 5us (microsecond) pulse to trigger
+	    # Send 5us pulse to trigger
             GPIO.output(GPIO_TRIGGER, False)
             time.sleep(0.000002)
             GPIO.output(GPIO_TRIGGER, True)
@@ -28,37 +26,37 @@ def measure():
             GPIO.output(GPIO_TRIGGER, False)
             start = time.time()
 
-            goodping=True
-            watchtime=time.time()
+            successful_ping=True
+            read_time=time.time()
             ping_start_time=0
             ping_end_time=0
-            while GPIO.input(GPIO_ECHO)==0 and goodping:
+            while GPIO.input(GPIO_ECHO)==0 and successful_ping:
                 ping_start_time = time.time()
-                if (ping_start_time-watchtime > ping_timeout):
-                    goodping=False
+                if (ping_start_time-read_time > ping_timeout):
+                    successful_ping=False
 
-            if goodping:
-                watchtime=time.time()
-                while GPIO.input(GPIO_ECHO)==1 and goodping:
+            if successful_ping:
+                read_time=time.time()
+                while GPIO.input(GPIO_ECHO)==1 and successful_ping:
                     ping_end_time=time.time()
-                    if (ping_end_time-watchtime > ping_timeout):
-                        goodping=False
+                    if (ping_end_time-read_time > ping_timeout):
+                        successful_ping=False
             else:
                 continue
 
-            if not goodping:
+            if not successful_ping:
                 continue
 
-            if goodping:
+            if successful_ping:
                 # Calculate pulse length
                 elapsed = ping_end_time - ping_start_time
 
                 # Get currrent air temp for speed of sound calculation
-                # air_temp = tmp102.read()
-                air_temp = 20
+                # temperature = tmp102.read()
+                temperature = 20
 
                 # Calculate current speed of sound based on air temp
-                sound_speed = 331.3 + (0.6 * air_temp)
+                sound_speed = 331.3 + (0.6 * temperature)
 
                 # Distance is pulse length multipled by the speed of sound
                 # Divide by two to avoid measuring distance twice
@@ -68,7 +66,6 @@ def measure():
                 # Add distance to the oil value to the array
                 distances.append(distance)
                 time.sleep(0.02)
-
                 complete=True
 
             time.sleep(0.05)
@@ -77,19 +74,19 @@ def measure():
     distances.sort
 
     # Chunk into thirds
-    onethird = int(len(distances) / 3)
-    twothirds = onethird * 2
+    third = int(len(distances) / 3)
+    two_thirds = third * 2
 
     # Find the distances in the middle 33%. Discard outliers that are too large
-    mid_distances = distances[onethird:twothirds]
+    middle_distances = distances[third:two_thirds]
 
     # Get the median by using reduce on the 'middle' range of distances
-    tank_air_space = (reduce(add, mid_distances)) / len(mid_distances)
+    oil_tank_space = (reduce(add, middle_distances)) / len(middle_distances)
 
     # Device accurate to 1cm so round the value to nearest cm
-    tank_air_space = round(tank_air_space)
+    oil_tank_space = round(oil_tank_space)
 
-    return tank_air_space
+    return oil_tank_space
 
 # For reduce add function later on to calculate sum of distances for averaging
 # Sum of first two items + the next item
@@ -125,16 +122,11 @@ time.sleep(0.5)
 # Various oil calculations
 distance = measure()
 oil_level = oil_tank_height - distance
-print "Oil remaining is {} cm".format(oil_level)
+print "Oil depth is {} cm".format(oil_level)
 percent_full = (oil_level / oil_tank_height) * 100
-print "Tank is {} percent full".format(percent_full)
-oil_litres_remaining = oil_tank_capacity * (oil_level/oil_tank_height)
-print "There is {} litres of oil remaining".format(oil_litres_remaining)
-oil_hours_remaining = oil_litres_remaining / oil_burn_rate
-print "There is {} hours of oil remaining".format(oil_hours_remaining)
-
-# Send percentage remaining value to virtual device in Domoticz
-urllib.urlopen("http://192.168.1.1:8080/json.htm?type=command&param=udevice&idx={}&nvalue=0&svalue={}".format( domoticz_device_idx, percent_full ))
+print "Oil tank is {}% full".format(percent_full)
+oil_litres_remaining = oil_tank_capacity * (oil_level / oil_tank_height)
+print "There is {} litres of oil in the tank".format(oil_litres_remaining)
 
 # GPIO reset
 GPIO.cleanup()
